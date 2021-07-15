7ea57df5a2a57171b4c27e8c3546c849379c7831
Aleksey Markin <traldar@mail.ru>
2021-07-15 13:34:19 +0300

    ДЗ 7

--- src/CatAndPlate/Cat.java
+++ src/CatAndPlate/Cat.java
@@ -2,7 +2,25 @@ package CatAndPlate;
 
 public class Cat {
 
-   
+    private String name;
+    private int appetite;
+    private boolean hungry;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.hungry = true;
+    }
+
+    public void info() {
+        String isHungry = !hungry ? "поел и наелся." : "не хватило еды и остался голоден.";
+        System.out.println(name + ": " + isHungry);
+    }
+
+    public void eat(Plate plate) {
+        if (hungry && plate.decreaseFood(appetite))
+            hungry = false;
+    }
 }
 
 
